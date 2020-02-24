ec9e602a3d050e49cf111b4fd8d39cc841576a51
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-24 17:32:26 -0500

    Moving a value to the front

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -93,7 +93,8 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        self.delete(node)
+        self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
