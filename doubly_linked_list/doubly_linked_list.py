6b0b2663ddb813b767128ed968ba372dd1864b2a
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-24 17:33:12 -0500

    Moving a value to the end

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -99,7 +99,8 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        self.delete(node)
+        self.add_to_tail(node.value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
