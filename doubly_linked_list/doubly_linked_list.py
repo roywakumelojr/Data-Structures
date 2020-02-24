8c75ba4a5f2c12ad38ba3e14bf02bfc978e8e345
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-24 17:29:49 -0500

    Removing a value from the head

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -65,7 +65,9 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        value = self.head.value
+        self.delete(self.head)
+        return value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
