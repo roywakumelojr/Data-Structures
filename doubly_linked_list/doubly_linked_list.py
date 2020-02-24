88e794d737546e593795e71e33a01caaea9e1296
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-24 17:31:49 -0500

    Removing a value from the tail

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -86,7 +86,9 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
