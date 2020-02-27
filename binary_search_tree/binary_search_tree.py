9792e5238d466d30e6258f1eaf8c9cd641f7988e
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-27 12:03:33 -0500

    Binary search tree complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,46 +12,82 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        """
-        #### Insert Value ####
-        If there is no node at root 
-        Compare value to the root
-        If value is smaller :
-            look left if node, repeat steps
-            if no node: Make new one with this value
-        If value is greater or equal
-            Look right, if node repeat steps
-            if no node: make new one with this value
-        """
-        pass
+        # #### Insert Value ####
+        # If there is no node at root
+        if not BinarySearchTree(value):
+            return None
+
+        # Compare value to the root
+        # If value is smaller :
+        # look left if node, repeat steps
+        elif value < self.value: 
+            if not self.left: 
+                self.left = BinarySearchTree(value)
+
+            #  if no node: Make new one with this value
+            else: 
+                self.left.insert(value)
+
+        # If value is greater or equal
+        # Look right, if node repeat steps
+        elif value >= self.value: 
+            if not self.right: 
+                self.right = BinarySearchTree(value)
+
+            #  if no node: Make new one with this value
+            else: 
+                self.right.insert(value) 
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        """
         #### Find Value ####
-        If no node at root, return false
-        Compare value to root
-        If smaller:
-            go left: look at node there
-        Is greater or ==:
-            go right
-        """
-        pass
+        # If no node at root, return false
+        if not BinarySearchTree(target):
+            return False
+
+        # Compare value to root
+        elif self.value == target:
+            return True
+
+        # If smaller:
+        #     go left: look at node there
+        elif target < self.value: 
+            if not self.left: 
+                return False
+            else: 
+                return self.left.contains(target)
+
+        # Is greater or ==:
+        #     go right
+        elif target >= self.value: 
+            if not self.right: 
+                return False
+            else: 
+                return self.right.contains(target)
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        """
         #### Get Max ####
-        If no right child, return this value
-        Otherwise, go right
-        """
-        pass
+        # If no right child, return this value
+        if not self.right: 
+            return self.value
+
+        # Otherwise, go right
+        else: 
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left:
+            self.left.for_each(cb)
+
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
